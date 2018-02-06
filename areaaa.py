8dfad74c26492ca6249e88928fa73e78842d9303
NaziaAnjumSharupa <nasxotic@gmail.com>
2018-02-06 19:07:04 +0600

    Initial commit

---
+++ areaaa.py
@@ -0,0 +1,22 @@
+base=10
+hight=20
+area=1/2*(base*hight)
+print("area is", area)
+print("done" , 's' + 'S')
+myname=input()
+print("mymane is" ,myname)
+
+name = 'Mary'
+password = 'swordfish'
+nam=input()
+if nam == 'Mary':
+    print('Hello Mary')
+elif nam == 'swordfish':
+    print('Access granted.')
+else:
+        print('Wrong password.')
+        name = 'Bob'
+        if name == 'Alice':
+            print('Hi, Alice.')
+        else:
+            print('Hello, stranger.')
---
+++ class_one.py
@@ -0,0 +1,4 @@
+def nnn(x,y):
+    z=x+y
+    return z
+print(nnn(4,5))
